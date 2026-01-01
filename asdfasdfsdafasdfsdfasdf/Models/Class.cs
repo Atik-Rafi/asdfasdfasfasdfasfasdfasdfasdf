e72a9f68ee0d94a02140362f14f2f68988ee20d1
Rafi <johncena152012@gmail.com>
2026-01-01 16:18:58 +0600

    adsfsdf

--- asdfasdfsdafasdfsdfasdf/Models/Class.cs
+++ asdfasdfsdafasdfsdfasdf/Models/Class.cs
@@ -6,4 +6,10 @@
         public int MyProperty2 { get; set; }
         public int MyProperty3 { get; set; }
     }
+    public class Class2
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+    }
 }
